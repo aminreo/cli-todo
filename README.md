d21f2374511161f35cd829a24a761c9d459a10a5
Amin <ayari.am@campus.lmu.de>
2026-09-10 17:00:20 +0200

    added commands `list` and `exit`, main loop implementation

--- README.md
+++ README.md
@@ -12,7 +12,24 @@ output
 Task added: task1
 Task added: task2
 ```
-
+# Commands
+* list
+```java
+list
+```
+output
+```console
+[task1, task2]
+```
+* exit
+```java
+exit
+```
+output
+```console
+Closing app...
+```
+Closes the app
 # Concepts used
 * Git
 * Collections
--- todoApp.java
+++ todoApp.java
@@ -1,12 +1,42 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class  TodoApp {
-    private static List<String> tasks=new ArrayList<>();
+public class TodoApp {
+    private static List<String> tasks = new ArrayList<>();
 
-    public static void main (String[] args ) {
+    public static void main(String[] args) {
         addTasks(args);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        System.out.println("=== CLI To-Do App ===");
+
+        while (running) {
+            String input = scanner.nextLine().trim();
+            if (input.isEmpty())
+                continue;
+            String[] parts = input.split("\\s+", 2);
+            String command = parts[0].toLowerCase();
+            String rest = parts.length > 1 ? parts[1] : "";
+
+            switch (command) {
+                case "list":
+                    System.out.println(tasks);
+                    break;
+                case "exit":
+                    System.out.println("Closing app...");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Unable to process: "+command +" " + rest);
+
+            }
+
+        }
+        scanner.close();
 
     }
 
@@ -18,7 +48,7 @@ public class  TodoApp {
     }
 
     private static void printTaskAddedInfo(String arg) {
-        System.out.println("Task added: "+ arg);
+        System.out.println("Task added: " + arg);
     }
 
 }
